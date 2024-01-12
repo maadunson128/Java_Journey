86305d0a7878f9662a4501b7a1a16db3802f3f41
maadunson <pradeepcoder2003@gmail.com>
2024-01-12 21:55:43 +0530

    Cgpa for GCT Regulation

---
+++ conditionals-loops/src/Cgpa.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Cgpa {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("NOTE : Enter 0 in to end the program \n");
+        System.out.println("Enter the Grade Points and Credit of the course : ");
+        double numer = 0;
+        double denom = 0;
+        while (true) {
+            int gd_pts = in.nextInt();
+            double credit = in.nextDouble();
+            if(gd_pts == 0 || credit == 0)
+            {
+                break;
+            }
+
+            else
+            {
+                numer = numer+ (gd_pts * credit);
+                denom = denom + credit;
+
+            }
+        }
+
+        System.out.println("CGPA : " + (numer/denom));
+    }
+}

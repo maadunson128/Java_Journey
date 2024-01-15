e19bfc53f3fef4d1cf5495c68773a3bb6d9d4239
maadunson <pradeepcoder2003@gmail.com>
2024-01-15 20:20:59 +0530

    Permutation and Combination

---
+++ conditionals-loops/src/Per_com.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Per_com
+{
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+
+        System.out.print("Enter the Total number of items(Objects) : ");
+        int n = in.nextInt();
+        System.out.print("Enter the number of selected items : ");
+        int r = in.nextInt();
+
+        System.out.println("Permutation : " + factor(n)/factor(n-r));
+        System.out.println("Permutation : " + factor(n)/(factor(n-r)*factor(r)));
+
+    }
+
+    static int factor(int n)
+    {
+        int fact = 1;
+
+        for(int i =1;i<=n;i++)
+        {
+            fact = fact*i;
+        }
+
+        return fact;
+
+
+    }
+}
